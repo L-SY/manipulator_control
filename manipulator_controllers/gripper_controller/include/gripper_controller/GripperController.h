11c015d9018a0af6612c538c9f9efa54fbaa2a95
LSY <2028238109@qq.com>
2024-08-20 14:23:04 +0800

    Use custom msgs to pub.

--- manipulator_controllers/gripper_controller/include/gripper_controller/GripperController.h
+++ manipulator_controllers/gripper_controller/include/gripper_controller/GripperController.h
@@ -4,51 +4,55 @@
 
 #pragma once
 
+#include <controller_interface/multi_interface_controller.h>
 #include <effort_controllers/joint_effort_controller.h>
 #include <effort_controllers/joint_position_controller.h>
 #include <effort_controllers/joint_velocity_controller.h>
-#include <controller_interface/multi_interface_controller.h>
 #include <hardware_interface/joint_command_interface.h>
 #include <realtime_tools/realtime_publisher.h>
 
 #include "std_msgs/Float64.h"
+#include "manipulator_msgs/GripperState.h"
+#include "manipulator_msgs/GripperCmd.h"
 
-namespace gripper_controller
-{
-class gripperController : public controller_interface::MultiInterfaceController<hardware_interface::EffortJointInterface>
-{
-  enum ControllerState {
-    NORMAL
-  };
+namespace gripper_controller {
+class gripperController : public controller_interface::MultiInterfaceController<
+                              hardware_interface::EffortJointInterface> {
+  enum ControllerState { NORMAL, SHUTDOWN };
+
 public:
   gripperController();
-  bool init(hardware_interface::RobotHW* robot_hw, ros::NodeHandle& root_nh, ros::NodeHandle& controller_nh) override;
-  void starting(const ros::Time& time) override;
-  void update(const ros::Time& time, const ros::Duration& period) override;
+  bool init(hardware_interface::RobotHW *robot_hw, ros::NodeHandle &root_nh,
+            ros::NodeHandle &controller_nh) override;
+  void starting(const ros::Time &time) override;
+  void update(const ros::Time &time, const ros::Duration &period) override;
 
   void getGains(double &p, double &i, double &d, double &i_max, double &i_min);
 
-  void getGains(double &p, double &i, double &d, double &i_max, double &i_min, bool &antiwindup);
+  void getGains(double &p, double &i, double &d, double &i_max, double &i_min,
+                bool &antiwindup);
 
-  void setGains(const double &p, const double &i, const double &d, const double &i_max, const double &i_min, const bool &antiwindup = false);
+  void setGains(const double &p, const double &i, const double &d,
+                const double &i_max, const double &i_min,
+                const bool &antiwindup = false);
 
 private:
-  void normal(const ros::Time& time, const ros::Duration& period);
-  void commandCB(const std_msgs::Float64 & msg);
+  void normal(const ros::Time &time, const ros::Duration &period);
+  void shutdown(const ros::Time &time, const ros::Duration &period);
+  void commandCB(const manipulator_msgs::GripperCmd& msg);
   void enforceJointLimits(double &command);
 
   int loopCount_;
   control_toolbox::Pid pidController_;
-  std::unique_ptr<
-      realtime_tools::RealtimePublisher<std_msgs::Float64> > statePub_ ;
 
+  std::unique_ptr<realtime_tools::RealtimePublisher<manipulator_msgs::GripperState>> statePub_;
   int controllerState_ = NORMAL;
   ros::Time startTime_;
   bool stateChanged_ = false;
   ros::Subscriber cmdSub_;
   hardware_interface::JointHandle jointHandle_;
   urdf::JointConstSharedPtr jointUrdf_;
-  realtime_tools::RealtimeBuffer<std_msgs::Float64> cmdRtBuffer_{};
+  realtime_tools::RealtimeBuffer<manipulator_msgs::GripperCmd> cmdRtBuffer_{};
 };
 
-}  // namespace gripper_controller
+} // namespace gripper_controller
--- manipulator_controllers/gripper_controller/src/GripperController.cpp
+++ manipulator_controllers/gripper_controller/src/GripperController.cpp
@@ -2,13 +2,13 @@
 // Created by lsy on 24-5-23.
 //
 
-#include <string>
 #include <pluginlib/class_list_macros.hpp>
+#include <string>
 
 #include "gripper_controller/GripperController.h"
 
 namespace gripper_controller {
-gripperController::gripperController() : loopCount_(0){}
+gripperController::gripperController() : loopCount_(0) {}
 
 bool gripperController::init(hardware_interface::RobotHW *robot_hw,
                              ros::NodeHandle &root_nh,
@@ -24,30 +24,33 @@ bool gripperController::init(hardware_interface::RobotHW *robot_hw,
   if (!pidController_.init(ros::NodeHandle(controller_nh, "pid")))
     return false;
 
-  statePub_.reset(
-      new realtime_tools::RealtimePublisher<std_msgs::Float64>(controller_nh, "state", 1));
+  statePub_.reset(new realtime_tools::RealtimePublisher<manipulator_msgs::GripperState>(
+      controller_nh, "state", 1));
 
   jointHandle_ = effortJointInterface->getHandle(jointHandle_name);
 
   cmdSub_ = controller_nh.subscribe("command", 1, &gripperController::commandCB,
                                     this);
+
   // Get URDF info about joint
   urdf::Model urdf;
-  if (!urdf.initParamWithNodeHandle("robot_description", root_nh))
-  {
+  if (!urdf.initParamWithNodeHandle("robot_description", root_nh)) {
     ROS_ERROR("Failed to parse urdf file");
     return false;
   }
   jointUrdf_ = urdf.getJoint(jointHandle_name);
-  if (!jointUrdf_)
-  {
+  if (!jointUrdf_) {
     ROS_ERROR("Could not find joint '%s' in urdf", jointHandle_name.c_str());
     return false;
   }
 
-  std_msgs::Float64 initPos;
-  initPos.data = jointHandle_.getPosition();
-  cmdRtBuffer_.initRT(initPos);
+  manipulator_msgs::GripperCmd initCmd;
+  initCmd.stamp = ros::Time::now();
+  initCmd.mode = NORMAL;
+  initCmd.des_pos = 0.;
+  initCmd.des_vel = 0.;
+  initCmd.des_eff = 0.;
+  cmdRtBuffer_.initRT(initCmd);
   return true;
 }
 
@@ -55,20 +58,21 @@ void gripperController::starting(const ros::Time & /*unused*/) {
   controllerState_ = ControllerState::NORMAL;
 }
 
-void gripperController::setGains(const double &p, const double &i, const double &d, const double &i_max, const double &i_min, const bool &antiwindup)
-{
-  pidController_.setGains(p,i,d,i_max,i_min,antiwindup);
+void gripperController::setGains(const double &p, const double &i,
+                                 const double &d, const double &i_max,
+                                 const double &i_min, const bool &antiwindup) {
+  pidController_.setGains(p, i, d, i_max, i_min, antiwindup);
 }
 
-void gripperController::getGains(double &p, double &i, double &d, double &i_max, double &i_min, bool &antiwindup)
-{
-  pidController_.getGains(p,i,d,i_max,i_min,antiwindup);
+void gripperController::getGains(double &p, double &i, double &d, double &i_max,
+                                 double &i_min, bool &antiwindup) {
+  pidController_.getGains(p, i, d, i_max, i_min, antiwindup);
 }
 
-void gripperController::getGains(double &p, double &i, double &d, double &i_max, double &i_min)
-{
+void gripperController::getGains(double &p, double &i, double &d, double &i_max,
+                                 double &i_min) {
   bool dummy;
-  pidController_.getGains(p,i,d,i_max,i_min,dummy);
+  pidController_.getGains(p, i, d, i_max, i_min, dummy);
 }
 
 void gripperController::update(const ros::Time &time,
@@ -86,54 +90,63 @@ void gripperController::update(const ros::Time &time,
   }
 }
 
-  void gripperController::normal(const ros::Time &time,
-                                 const ros::Duration &period) {
-//    ROS_INFO_STREAM("normal Mode");
-    double command_position = (cmdRtBuffer_.readFromRT())->data;
+void gripperController::shutdown(const ros::Time &time,
+                                 const ros::Duration &period) {}
 
-    double error;
-    double commanded_effort;
+void gripperController::normal(const ros::Time &time,
+                               const ros::Duration &period) {
+  //    ROS_INFO_STREAM("normal Mode");
+  double des_position = (cmdRtBuffer_.readFromRT())->des_pos;
+  double des_velocity = (cmdRtBuffer_.readFromRT())->des_vel;
+  double des_effort = (cmdRtBuffer_.readFromRT())->des_eff;
 
-    double current_position = jointHandle_.getPosition();
+  double posErr, velErr;
+  double commanded_effort;
 
-    enforceJointLimits(command_position);
+  double current_position = jointHandle_.getPosition();
+  double current_velocity = jointHandle_.getVelocity();
 
-    error = command_position - current_position;
-    commanded_effort = pidController_.computeCommand(error, period);
+  enforceJointLimits(des_position);
 
-    jointHandle_.setCommand(commanded_effort);
+  posErr = des_position - current_position;
+  velErr = des_velocity - current_velocity;
 
-    if (loopCount_ % 10 == 0)
-    {
-      if(statePub_ && statePub_->trylock())
-      {
-        statePub_->msg_.data = jointHandle_.getPosition();
-        statePub_->unlockAndPublish();
-      }
-    }
-    loopCount_++;
-  }
+  commanded_effort = pidController_.computeCommand(posErr, velErr,period) + des_effort;
 
-  void gripperController::enforceJointLimits(double &command)
-  {
-    // Check that this joint has applicable limits
-    if (jointUrdf_->type == urdf::Joint::REVOLUTE || jointUrdf_->type == urdf::Joint::PRISMATIC)
-    {
-      if( command > jointUrdf_->limits->upper ) // above upper limnit
-      {
-        command = jointUrdf_->limits->upper;
-      }
-      else if( command < jointUrdf_->limits->lower ) // below lower limit
-      {
-        command = jointUrdf_->limits->lower;
-      }
+  jointHandle_.setCommand(commanded_effort);
+
+  if (loopCount_ % 10 == 0) {
+    if (statePub_ && statePub_->trylock()) {
+      statePub_->msg_.mode = controllerState_;
+      statePub_->msg_.stamp = time;
+      statePub_->msg_.gripper_pos = jointHandle_.getPosition();
+      statePub_->msg_.gripper_vel = jointHandle_.getVelocity();
+      statePub_->msg_.gripper_eff = jointHandle_.getEffort();
+      statePub_->unlockAndPublish();
     }
   }
+  loopCount_++;
+}
 
-  void gripperController::commandCB(const std_msgs::Float64 &msg) {
-    cmdRtBuffer_.writeFromNonRT(msg);
+void gripperController::enforceJointLimits(double &command) {
+  // Check that this joint has applicable limits
+  if (jointUrdf_->type == urdf::Joint::REVOLUTE ||
+      jointUrdf_->type == urdf::Joint::PRISMATIC) {
+    if (command > jointUrdf_->limits->upper) // above upper limnit
+    {
+      command = jointUrdf_->limits->upper;
+    } else if (command < jointUrdf_->limits->lower) // below lower limit
+    {
+      command = jointUrdf_->limits->lower;
+    }
   }
+}
 
-} //namespace gripper_controller
+void gripperController::commandCB(const manipulator_msgs::GripperCmd& msg) {
+  cmdRtBuffer_.writeFromNonRT(msg);
+}
 
-PLUGINLIB_EXPORT_CLASS(gripper_controller::gripperController, controller_interface::ControllerBase)
+} // namespace gripper_controller
+
+PLUGINLIB_EXPORT_CLASS(gripper_controller::gripperController,
+                       controller_interface::ControllerBase)
