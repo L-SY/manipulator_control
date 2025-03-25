9543db8fbf58ab71a190a3a0cea867ef1ff983f8
LSY <2028238109@qq.com>
2025-03-25 17:11:20 +0800

    Add loadUrdf().

--- manipulator_assets/swingarm_v1/swingarm_hw/src/hardware_interface.cpp
+++ manipulator_assets/swingarm_v1/swingarm_hw/src/hardware_interface.cpp
@@ -8,10 +8,10 @@ namespace SwingArm {
 bool SwingArmHW::init(ros::NodeHandle& root_nh, ros::NodeHandle& robot_hw_nh) {
   canManager_ = std::make_shared<device::CanManager>(robot_hw_nh);
 
-//  if (!loadUrdf(root_nh)) {
-//    ROS_ERROR("Error occurred while setting up urdf");
-//    return false;
-//  }
+  if (!loadUrdf(root_nh)) {
+    ROS_ERROR("Error occurred while setting up urdf");
+    return false;
+  }
 
   setupJoints();
   setupImus();
@@ -107,4 +107,15 @@ bool SwingArmHW::setupImus() {
   }
   return true;
 }
+
+bool SwingArmHW::loadUrdf(ros::NodeHandle& rootNh) {
+  std::string urdfString;
+  if (urdfModel_ == nullptr) {
+    urdfModel_ = std::make_shared<urdf::Model>();
+  }
+  // get the urdf param on param server
+  rootNh.getParam("robot_description", urdfString);
+  return !urdfString.empty() && urdfModel_->initString(urdfString);
+}
+
 }  // namespace SwingArm
